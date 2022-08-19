95a43bd9cac494303ae79c9b7dbb5158cd405ab9
frederic <jpolivierr@gmail.com>
2022-08-19 14:13:58 -0400

    edited account Servlet

--- WEB-INF/classes/net/greenbudget/servlets/accountServlet.java
+++ WEB-INF/classes/net/greenbudget/servlets/accountServlet.java
@@ -1,5 +1,16 @@
 package net.greenbudget.servlets;
 
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 public class accountServlet {
-    
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+     
+        String account = req.getParameter("account");
+        resp.getWriter().println(account);
+
+}
 }
