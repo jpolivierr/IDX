2e40608c52d91c613b69424297fa384c4d9ce952
frederic <jpolivierr@gmail.com>
2022-08-26 11:55:04 -0400

    Completed delete transaction class

--- WEB-INF/classes/net/greenbudget/db/methods/DeleteTransaction.java
+++ WEB-INF/classes/net/greenbudget/db/methods/DeleteTransaction.java
@@ -1,5 +1,66 @@
 package net.greenbudget.db.methods;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import net.greenbudget.Config.DbConfig;
+import net.greenbudget.response.Response;
+
 public class DeleteTransaction {
-    
+    private static DeleteTransaction  instance;
+
+    private DeleteTransaction(){}
+
+    public static DeleteTransaction  getInstance(){
+        return instance = instance == null ? new DeleteTransaction () : null;
+    }
+
+    public String init(DbConnection dbConnection, Integer transactionId, String userEmail){
+
+        //Get the connection
+        Connection connection = dbConnection.connect();
+        String response = null;
+
+        try {
+            // get the Query string form .env
+            String query = new DbConfig().getQueryDeleteTransaction();
+
+            //create prepare statement
+            dbConnection.pstmt = connection.prepareStatement(query);
+
+            dbConnection.pstmt.setInt(1, transactionId);
+            dbConnection.pstmt.setString(2, userEmail); 
+            
+            dbConnection.pstmt.execute();
+
+             //Create a responses
+            var jsonResponse = new Response(200, "Expenses Deleted.", null);
+            //assign response
+                response = jsonResponse.send();
+
+            
+        }catch (SQLException e) {
+            //Create a responses
+            var jsonResponse = new Response(406, DbExceptionHandler.Message(e.getErrorCode(),e.getMessage()), null);
+
+            //assign response
+            response = jsonResponse.send();
+
+       }catch(Exception e){
+           //Create a responses
+           var jsonResponse = new Response(406, e.getMessage(), null);
+
+           //assign response
+           response = jsonResponse.send();
+
+       }finally{
+            try {
+                connection.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return response;
+    }
 }
